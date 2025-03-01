ddffed250ed40df570ed8b7d0fa3c3a30ceeae4a
Ziga X <shehu.meni@gmail.com>
2025-03-02 00:45:38 +0100

    Added explanation for markdown plugin

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,3 +1,13 @@
+-- Markdown Preview Plugin for LazyVim
+-- This plugin provides a live Markdown preview in the browser.
+-- It automatically starts the preview when opening a Markdown file.
+
+-- Usage:
+-- - Open a Markdown file in Neovim (`nvim myfile.md`).
+-- - Run `:MarkdownPreview` to start the preview in your web browser.
+-- - Run `:MarkdownPreviewStop` to stop the preview.
+-- - If auto-start is enabled (`vim.g.mkdp_auto_start = 1`), it will start automatically.
+
 return {
   {
     "iamcco/markdown-preview.nvim",
